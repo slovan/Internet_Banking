034d2d3e0d15ad35ad9105d7d11c100d2630a24c
Volodymyr Ponomarenko <volod.ponomar@gmail.com>
2015-12-05 13:18:03 +0100

    Create AccountGenerator class
    Put all methods generating accounts numbers in this class. Add method to
    generate IBAN number of account

---
+++ VPBank/src/com/vpbank/services/AccountGenerator.java
@@ -0,0 +1,273 @@
+/**
+ * 
+ */
+package com.vpbank.services;
+
+import java.util.Date;
+
+import com.vpbank.models.Client;
+
+/**
+ * @author Volodymyr Ponomarenko
+ *
+ */
+// special class for automatic generation of account numbers
+public class AccountGenerator {
+	// generate individual account number (basic)
+	public static long genAccountNumberBasic(Client c) {
+		int firstNameLength = c.getFirstName().length(); // take first name
+															// length
+		int lastNameLength = c.getLastName().length(); // take last name length
+		int id = c.getId(); // take id number
+		long birthdayNum = c.getDob().getTime(); // take date of birth in number
+		long todayNum = new Date().getTime(); // take current date in number
+
+		long accountNumberBasic = ((todayNum - birthdayNum) / id) % 10000000000L; // initialization
+		accountNumberBasic = (accountNumberBasic * firstNameLength * lastNameLength) % 10000000000L;
+
+		// According to the National Bank of Slovakia, to validate the basic
+		// part
+		// of the account number, the digits are multiplied by
+		// their respective weights (6,3,7,9,10,5,8,4,2,1), added up and divided
+		// by 11.
+		// The remainder must be zero.
+
+		byte[] accountNumArray = new byte[10]; // because 10 digits of account
+		accountNumArray[0] = (byte) (accountNumberBasic / 1000000000); // get
+																		// 1st
+																		// digit
+																		// from
+																		// the
+																		// number
+		for (int i = 1; i < accountNumArray.length; i++) {
+			// get 2nd, 3rd, ...., 10th digits from the number
+			accountNumArray[i] = (byte) ((accountNumberBasic - accountNumArray[i - 1] * Math.pow(10, (10 - i)))
+					/ Math.pow(10, (10 - i - 1)));
+		}
+		byte[] weights = { 6, 3, 7, 9, 10, 5, 8, 4, 2, 1 };
+
+		// check remainder
+		int remainder = 0;
+		for (int i = 0; i < accountNumArray.length; i++) {
+			remainder += accountNumArray[i] * weights[i];
+		}
+		remainder %= 11;
+		if ((remainder == 0) && (accountNumberBasic != 0))
+			return accountNumberBasic;
+		else {
+			// we have to get reminder = 0, use loop for this
+			do {
+				switch (remainder) {
+				// increase with 10, because of value of weight
+				case 1:
+					accountNumArray[4] = (byte) (++accountNumArray[4] % 10);
+					break;
+				case 2:
+					accountNumArray[3] = (byte) (++accountNumArray[3] % 10);
+					break;// increase with 9
+				case 3:
+					accountNumArray[6] = (byte) (++accountNumArray[6] % 10);
+					break;// increase with 8
+				case 4:
+					accountNumArray[2] = (byte) (++accountNumArray[2] % 10);
+					break;// increase with 7
+				case 5:
+					accountNumArray[0] = (byte) (++accountNumArray[0] % 10);
+					break;// increase with 6
+				case 6:
+					accountNumArray[5] = (byte) (++accountNumArray[5] % 10);
+					break;// increase with 5
+				case 7:
+					accountNumArray[7] = (byte) (++accountNumArray[7] % 10);
+					break;// increase with 4
+				case 8:
+					accountNumArray[1] = (byte) (++accountNumArray[1] % 10);
+					break;// increase with 3
+				case 9:
+					accountNumArray[8] = (byte) (++accountNumArray[8] % 10);
+					break;// increase with 2
+				case 10:
+					accountNumArray[9] = (byte) (++accountNumArray[9] % 10);
+					break;// increase with 1
+				default:
+					accountNumArray[0] = 1;// in case of (accountNumberBasic ==
+											// 0)
+				}
+				remainder = 0;
+				for (int i = 0; i < accountNumArray.length; i++) {
+					remainder += accountNumArray[i] * weights[i];
+				}
+				remainder %= 11;
+			} while (remainder != 0);
+
+			// take correct accountNumberBasic from array of byte
+			accountNumberBasic = 0;
+			for (int i = 0; i < accountNumArray.length; i++) {
+				accountNumberBasic += accountNumArray[i] * Math.pow(10, (9 - i));
+			}
+			return accountNumberBasic;
+		}
+	}
+
+	// generate individual account number (prefix)
+	public static int genAccountNumberPrefix(Client c, int start) {
+		// According to the National Bank of Slovakia, to validate the prefix
+		// part
+		// of the account number, the digits are multiplied by
+		// their respective weights (10,5,8,4,2,1), added up and divided by 11.
+		// The remainder must be zero.
+		int accountNumberPrefix = start + 1;
+
+		byte[] accountNumArray = new byte[6]; // because 6 digits of account
+		accountNumArray[0] = (byte) (accountNumberPrefix / 100000); // get 1st
+																	// digit
+																	// from the
+																	// number
+		for (int i = 1; i < accountNumArray.length; i++) {
+			// get 2nd, 3rd, ...., 10th digits from the number
+			accountNumArray[i] = (byte) ((accountNumberPrefix - accountNumArray[i - 1] * Math.pow(10, (6 - i)))
+					/ Math.pow(10, (6 - i - 1)));
+		}
+		byte[] weights = { 10, 5, 8, 4, 2, 1 };
+
+		// check remainder
+		int remainder = 0;
+		for (int i = 0; i < accountNumArray.length; i++) {
+			remainder += accountNumArray[i] * weights[i];
+		}
+		remainder %= 11;
+		if (remainder == 0)
+			return accountNumberPrefix;
+		else {
+			// we have to get reminder = 0, use loop for this
+			do {
+				switch (remainder) {
+				case 1:
+					accountNumArray[0] = (byte) (++accountNumArray[0] % 10);
+					break;// increase with 10
+				case 2: {
+					// increase with 9
+					accountNumArray[1] = (byte) (++accountNumArray[1] % 10);// with
+																			// 5
+					accountNumArray[3] = (byte) (++accountNumArray[3] % 10);// with
+																			// 4
+					break;
+				}
+				case 3:
+					accountNumArray[2] = (byte) (++accountNumArray[2] % 10);
+					break;// increase with 8
+				case 4: {
+					// increase with 7
+					accountNumArray[1] = (byte) (++accountNumArray[1] % 10);// with
+																			// 5
+					accountNumArray[4] = (byte) (++accountNumArray[4] % 10);// with
+																			// 2
+					break;
+				}
+				case 5: {
+					// increase with 6
+					accountNumArray[3] = (byte) (++accountNumArray[3] % 10);// with
+																			// 4
+					accountNumArray[4] = (byte) (++accountNumArray[4] % 10);// with
+																			// 2
+					break;
+				}
+				case 6:
+					accountNumArray[1] = (byte) (++accountNumArray[1] % 10);
+					break;// increase with 5
+				case 7:
+					accountNumArray[3] = (byte) (++accountNumArray[3] % 10);
+					break;// increase with 4
+				case 8: {
+					// increase with 3
+					accountNumArray[5] = (byte) (++accountNumArray[5] % 10);// with
+																			// 1
+					accountNumArray[4] = (byte) (++accountNumArray[4] % 10);// with
+																			// 2
+					break;
+				}
+				case 9:
+					accountNumArray[4] = (byte) (++accountNumArray[4] % 10);
+					break;// increase with 2
+				case 10:
+					accountNumArray[5] = (byte) (++accountNumArray[5] % 10);
+					break;// increase with 1
+				}
+				remainder = 0;
+				for (int i = 0; i < accountNumArray.length; i++) {
+					remainder += accountNumArray[i] * weights[i];
+				}
+				remainder %= 11;
+			} while (remainder != 0);
+
+			// take correct accountNumberPrefix from array of byte
+			accountNumberPrefix = 0;
+			for (int i = 0; i < accountNumArray.length; i++) {
+				accountNumberPrefix += accountNumArray[i] * Math.pow(10, (6 - i - 1));
+			}
+			return accountNumberPrefix;
+		}
+	}
+
+	// generate IBAN form of the account number
+	public static String genAccountNumberIBAN(long accountNumberBasic, int accountNumberPrefix, short bankCode,
+			String countryCode) {
+		// convert country code to a number form and write it accountNumberIBAN
+		// variable
+		String accountNumberIBAN = "";
+		for (int i = 0; i < countryCode.length(); i++) {
+			// Replace each letter in the string with two digits,
+			// thereby expanding the string, where A = 10, B = 11, ..., Z = 35
+			accountNumberIBAN = accountNumberIBAN + ((countryCode.charAt(i) - 'A') + 10);
+		}
+		// add 2 zeros at the end of the string ("check digits")
+		accountNumberIBAN = accountNumberIBAN + "00";
+
+		// add account basic number to IBAN row
+		// at first, it needs exactly 10 symbols and we have to provide it
+		String strBasic = Long.toString(accountNumberBasic);
+		while (strBasic.length() < 10) {
+			strBasic = "0" + strBasic;
+		}
+		accountNumberIBAN = strBasic + accountNumberIBAN;
+
+		// add account prefix number to IBAN row
+		// at first, it needs exactly 6 symbols and we have to provide it
+		String strPrefix = Integer.toString(accountNumberPrefix);
+		while (strPrefix.length() < 6) {
+			strPrefix = "0" + strPrefix;
+		}
+		accountNumberIBAN = strPrefix + accountNumberIBAN;
+
+		// add bank code to IBAN row
+		// at first, it needs exactly 4 symbols and we have to provide it
+		String strBankCode = Short.toString(bankCode);
+		while (strBankCode.length() < 4) {
+			strBankCode = "0" + strBankCode;
+		}
+		accountNumberIBAN = strBankCode + accountNumberIBAN;
+
+		// compute the remainder of number in accountNumberIBAN on division by
+		// 97
+		// this number is bigger than Long type can contain, so divide it to 2
+		// parts
+		// first (last 18 symbols)
+		long first = Long
+				.parseLong(accountNumberIBAN.substring(accountNumberIBAN.length() - 18, accountNumberIBAN.length()));
+		// second
+		long second = Long.parseLong(accountNumberIBAN.substring(0, accountNumberIBAN.length() - 18));
+		// let's compute remainder
+		int remainder = (int) (first % 97 + (second * (Math.pow(10, 18) % 97)) % 97) % 97;
+
+		// compute check digits
+		String checkDigits = Integer.toString(98 - remainder);
+		// must consists of 2 symbols
+		if (checkDigits.length() == 1)
+			checkDigits = "0" + checkDigits;
+
+		// now let's form correct accountNumberIBAN
+		accountNumberIBAN = countryCode + checkDigits + strBankCode + strPrefix + strBasic;
+
+		return accountNumberIBAN;
+	}
+}
